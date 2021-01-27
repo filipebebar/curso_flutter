2bbbfb5d2abdba7e7623e608f189824e3076e1be
filipe <filipebe.barbosa@hotmail.com>
2021-01-26 22:25:57 -0300

    #Pagina dois

--- coipando_ghipy_gifs/lib/main.dart
+++ coipando_ghipy_gifs/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:coipando_ghipy_gifs/ui/home_page.dart';
+import 'package:coipando_ghipy_gifs/ui/gif_page.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
   ));
 }
